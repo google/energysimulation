3917cf39ab93f6df87e1e25d5f0d275d89ad14ff
Orion Pritchard <orionp@google.com>
2017-03-20 14:00:54 -0400

    Fixed comments in copyright so that cross would recognize it.
    Change-Id: I2839498af0f7bbc31ba977fbd04c7f84e66ba552

--- gridsim/__init__.py
+++ gridsim/__init__.py
@@ -1,10 +1,24 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Linear Program Constraints to simulate different energy assumptions.
 
 This package was originally written to support understanding of energy
 solutions within Google Research.  It has subsequently been used to
 generate scenarios for a website
 
-(TODO:(orionp) Link to website)
+http://energystrategies.org
 
 to help show policy makers, and other interested people, the cost
 optimal way to reduce CO2 emissions from the electricity sector.  This
--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2017 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.Copyright [yyyy] [name of copyright owner]
+
 from distutils.core import setup
 
 setup(
@@ -6,6 +20,8 @@ setup(
     author='Orion Pritchard',
     author_email='orionp@google.com',
     packages=['gridsim', 'gridsim.test'],
+    package_dir={'gridsim': 'gridsim'},
+    package_data={'gridsim': ['data/profiles/profiles_*.csv', 'data/costs/*.csv']},
     scripts=[],
     url='http://www.google.com',
     license='LICENSE',
